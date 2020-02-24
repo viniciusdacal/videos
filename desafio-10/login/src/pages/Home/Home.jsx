3d92b6597891c1014cccdf90fe0e6459948ae64e
Vinicius Dacal <viniciusldacal@gmail.com>
2020-02-24 02:13:18 -0300

    update home

--- desafio-10/login/src/pages/Home/Home.jsx
+++ desafio-10/login/src/pages/Home/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Home.css';
 
-const PagesHome = () => {
-
-  return (
-    <div className="pages-home">
-      Parabéns, você conseguiu
-      <br />
-      <button type="button">Sair</button>
-    </div>
-  );
-}
+const PagesHome = () => (
+  <div className="pages-home">
+    Parabéns, você conseguiu
+    <br />
+    <button type="button">Sair</button>
+  </div>
+);
 
 export default PagesHome;
